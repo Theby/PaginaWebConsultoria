5677783cb2f61a77e3c8eb59fa5bbc91f4750d49
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-07-13 17:39:02 -0400

    testing link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
+Rails.application.routes.draw do 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
+
+  # Ruta al index de la página web
+  get 'welcome/index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
