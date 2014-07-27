496e63ad486b0f9b69d67e6d559a2e52eb487798
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-07-27 04:08:22 -0400

    TEST: botones automaticos para trayectoria laboral

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :es_trayectoria_laboral?
+
   def es_trayectoria_laboral?
   	if action_name == "trayectoria_laboral"
   		true
