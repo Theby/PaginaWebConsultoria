f9a1bc060f1a2f0959cd901f480474f2233d2458
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-07-27 05:20:26 -0400

    TEST: variables en vistas para proyectos de trayectorias

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  attr_accessor :num_area
 
   helper_method :es_trayectoria_laboral
   helper_method :es_trayectoria_laboral_privado
