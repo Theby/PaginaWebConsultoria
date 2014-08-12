399eacd04cf77f4c066d1903e5dcc305dca1850c
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-08-12 01:38:27 -0400

    TEST: proyectos relacionados

--- app/controllers/contratanos_controller.rb
+++ app/controllers/contratanos_controller.rb
@@ -2,7 +2,7 @@ class ContratanosController < ApplicationController
   def contratanos
   	@num_service = params[:num_service]
 
-  	if params[:num_service] == 1
+  	if @num_service.to_i == 1
   		@proyectos = 3
   	else
   		@proyectos = 1
