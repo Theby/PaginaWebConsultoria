520f355241e707717a73667ce6eb64c8e2ff1274
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-08-12 01:15:32 -0400

    ADD: proyectos asociados a servicios

--- app/controllers/contratanos_controller.rb
+++ app/controllers/contratanos_controller.rb
@@ -1,5 +1,11 @@
 class ContratanosController < ApplicationController
   def contratanos
   	@num_service = params[:num_service]
+
+  	if @num_service == 1
+  		@proyectos = 8
+  	else
+  		@proyectos = 0
+  	end
   end
 end
