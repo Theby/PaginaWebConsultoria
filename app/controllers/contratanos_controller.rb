ba2ae45681d30ec3f9081786049ff076fff38377
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-08-12 01:26:55 -0400

    TEST: proyectos relacionados

--- app/controllers/contratanos_controller.rb
+++ app/controllers/contratanos_controller.rb
@@ -1,16 +1,7 @@
 class ContratanosController < ApplicationController
-
-  helper_method :num_proyectos
-
-  def num_proyectos(servicio)
-  	if servicio == 1
-  		return 8
-  	else
-  		return 0
-  	end
-  end
-
   def contratanos
   	@num_service = params[:num_service]
+
+  	qproyectos = 8
   end
 end
