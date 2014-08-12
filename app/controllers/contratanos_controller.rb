68aa7c56ce34078cc727d6475a392dd6462671a7
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-08-12 01:30:57 -0400

    TEST: proyectos relacionados

--- app/controllers/contratanos_controller.rb
+++ app/controllers/contratanos_controller.rb
@@ -2,6 +2,10 @@ class ContratanosController < ApplicationController
   def contratanos
   	@num_service = params[:num_service]
 
-  	@proyectos = 8
+  	@proyectos = 1
+
+  	if @num_service == 1
+  		@proyectos = 3
+  	end
   end
 end
