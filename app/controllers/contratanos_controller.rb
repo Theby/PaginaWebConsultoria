64f93ea1cd580e92ff8971e3aae10fbc375f413c
Esteban Gaete Flores <zoraskiller@gmail.com>
2014-08-12 01:57:51 -0400

    NEW: proyectos relacionados a servicios

--- app/controllers/contratanos_controller.rb
+++ app/controllers/contratanos_controller.rb
@@ -4,7 +4,21 @@ class ContratanosController < ApplicationController
 
   	if @num_service.to_i == 1
   		@proyectos = 3
-  	else
+  	elsif @num_service.to_i == 2
+  		@proyectos = 9
+  	elsif @num_service.to_i == 3
+  		@proyectos = 1
+  	elsif @num_service.to_i == 4
+  		@proyectos = 4
+  	elsif @num_service.to_i == 5
+  		@proyectos = 1
+  	elsif @num_service.to_i == 6
+  		@proyectos = 2
+  	elsif @num_service.to_i == 7
+  		@proyectos = 2
+  	elsif @num_service.to_i == 8
+  		@proyectos = 4
+  	elsif @num_service.to_i == 9
   		@proyectos = 1
   	end
   end
